081c163aac9705926715ab326465ea6d769d9bad
Davide Bianchi <davide.bianchi@mondora.com>
2015-11-13 16:20:00 +0100

    Added getServiceConfig function for getOauthMixin

--- src/asteroid-collections.js
+++ src/asteroid-collections.js
@@ -1,10 +1,23 @@
 import Immutable from "immutable";
 
-import {added, changed, removed} from "./handlers.js";
+import {added, changed, removed} from "./handlers";
+
+const SERVICE_CONFIG_COLLECTION = "meteor_accounts_loginServiceConfiguration";
 
 export function init () {
     this.collections = Immutable.Map();
     this.ddp.on("added", added.bind(this));
     this.ddp.on("changed", changed.bind(this));
     this.ddp.on("removed", removed.bind(this));
+}
+
+export function getServiceConfig (providerName) {
+    const serviceConfiguration = this.collections.getIn([
+        SERVICE_CONFIG_COLLECTION,
+        providerName
+    ]);
+    if (!serviceConfiguration) {
+        throw new Error(`No configuration found for provider ${providerName}`);
+    }
+    return serviceConfiguration.toJS();
 }
--- test/unit/asteroid-collections.js
+++ test/unit/asteroid-collections.js
@@ -10,80 +10,134 @@ import * as asteroidCollections from "asteroid-collections";
 
 describe("`asteroid-collection` mixin", function () {
 
-    const added = sinon.spy();
-    const changed = sinon.spy();
-    const removed = sinon.spy();
+    describe("`init` method", function () {
 
-    beforeEach(function () {
-        added.reset();
-        asteroidCollections.__Rewire__("added", added);
-        changed.reset();
-        asteroidCollections.__Rewire__("changed", changed);
-        removed.reset();
-        asteroidCollections.__Rewire__("removed", removed);
-    });
+        const added = sinon.spy();
+        const changed = sinon.spy();
+        const removed = sinon.spy();
 
-    afterEach(function () {
-        asteroidCollections.__ResetDependency__("added");
-        asteroidCollections.__ResetDependency__("changed");
-        asteroidCollections.__ResetDependency__("removed");
-    });
+        beforeEach(function () {
+            added.reset();
+            asteroidCollections.__Rewire__("added", added);
+            changed.reset();
+            asteroidCollections.__Rewire__("changed", changed);
+            removed.reset();
+            asteroidCollections.__Rewire__("removed", removed);
+        });
 
-    it("should set the `collections` object as `Immutable.Map`", function () {
-        const instance = {
-            ddp: {
-                on: sinon.spy()
-            }
-        };
-        asteroidCollections.init.call(instance);
-        expect(instance.collections).to.be.an.instanceOf(Immutable.Map);
-        expect(instance.collections.toJS()).to.deep.equal({});
-    });
+        afterEach(function () {
+            asteroidCollections.__ResetDependency__("added");
+            asteroidCollections.__ResetDependency__("changed");
+            asteroidCollections.__ResetDependency__("removed");
+        });
 
-    describe("`added` event handler", function () {
-
-        it("should call the `added` function on correct instance", function () {
-            var eventEmitter = new EventEmitter();
+        it("should set the `collections` object as `Immutable.Map`", function () {
             const instance = {
-                ddp: eventEmitter
+                ddp: {
+                    on: sinon.spy()
+                }
             };
             asteroidCollections.init.call(instance);
-            instance.ddp.emit("added");
-            expect(added).to.have.been.callCount(1);
-            expect(added).to.have.been.calledOn(instance);
+            expect(instance.collections).to.be.an.instanceOf(Immutable.Map);
+            expect(instance.collections.toJS()).to.deep.equal({});
+        });
+
+        describe("`added` event handler", function () {
+
+            it("should call the `added` function on correct instance", function () {
+                var eventEmitter = new EventEmitter();
+                const instance = {
+                    ddp: eventEmitter
+                };
+                asteroidCollections.init.call(instance);
+                instance.ddp.emit("added");
+                expect(added).to.have.been.callCount(1);
+                expect(added).to.have.been.calledOn(instance);
+            });
+
+        });
+
+        describe("`changed` event handler", function () {
+
+            it("should call the `changed` function on correct instance", function () {
+                var eventEmitter = new EventEmitter();
+                const instance = {
+                    ddp: eventEmitter
+                };
+                asteroidCollections.init.call(instance);
+                instance.ddp.emit("changed");
+                expect(changed).to.have.been.callCount(1);
+                expect(changed).to.have.been.calledOn(instance);
+            });
+
+        });
+
+        describe("`removed` event handler", function () {
+
+            it("should call the `removed` function on correct instance", function () {
+                var eventEmitter = new EventEmitter();
+                const instance = {
+                    ddp: eventEmitter
+                };
+                asteroidCollections.init.call(instance);
+                instance.ddp.emit("removed");
+                expect(removed).to.have.been.callCount(1);
+                expect(removed).to.have.been.calledOn(instance);
+            });
+
         });
 
     });
 
-    describe("`changed` event handler", function () {
+    describe("`getServiceConfig`", function () {
 
-        it("should call the `changed` function on correct instance", function () {
-            var eventEmitter = new EventEmitter();
+        const providerName = "providerName";
+        const SERVICE_CONFIG_COLLECTION = "SERVICE_CONFIG_COLLECTION";
+
+        before(function () {
+            asteroidCollections.__Rewire__("SERVICE_CONFIG_COLLECTION", SERVICE_CONFIG_COLLECTION);
+        });
+
+        after(function () {
+            asteroidCollections.__ResetDependency__("SERVICE_CONFIG_COLLECTION");
+        });
+
+        it("should call the `getIn` function of collections with the correct parameter", function () {
             const instance = {
-                ddp: eventEmitter
+                collections: {
+                    getIn: sinon.stub().returns(Immutable.Map())
+                }
             };
-            asteroidCollections.init.call(instance);
-            instance.ddp.emit("changed");
-            expect(changed).to.have.been.callCount(1);
-            expect(changed).to.have.been.calledOn(instance);
+            asteroidCollections.getServiceConfig.call(instance, providerName);
+            expect(instance.collections.getIn).to.have.callCount(1);
+            expect(instance.collections.getIn).to.have.been.calledWith([
+                SERVICE_CONFIG_COLLECTION,
+                providerName
+            ]);
+        });
+
+        it("should throw an `Error` with correct message if serviceConfiguration is `undefined`", function () {
+            const instance = {
+                collections: Immutable.Map()
+            };
+            const troubleMaker = () => {
+                asteroidCollections.getServiceConfig.call(instance, providerName);
+            };
+            // expect(ret).to.be.an.instanceOf(Error);
+            expect(troubleMaker).to.throw("No configuration found for provider providerName");
+        });
+
+        it("should return an object", function () {
+            const instance = {
+                collections: {
+                    getIn: sinon.stub().returns(Immutable.Map())
+                }
+            };
+            var ret = asteroidCollections.getServiceConfig.call(instance, providerName);
+            expect(ret).to.be.an.instanceOf(Object);
+            expect(ret).to.deep.equal({});
         });
 
     });
-
-    describe("`removed` event handler", function () {
-
-        it("should call the `removed` function on correct instance", function () {
-            var eventEmitter = new EventEmitter();
-            const instance = {
-                ddp: eventEmitter
-            };
-            asteroidCollections.init.call(instance);
-            instance.ddp.emit("removed");
-            expect(removed).to.have.been.callCount(1);
-            expect(removed).to.have.been.calledOn(instance);
-        });
-
-    });
-
 
 });
